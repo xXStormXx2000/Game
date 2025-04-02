7f87bd825b9fde2ea0320e16d746f7006eb14463
Storm wahlmann <47405034+xXStormXx2000@users.noreply.github.com>
2025-04-02 14:27:10 +0200

    opps

--- TestGame/Controler.cpp
+++ TestGame/Controler.cpp
@@ -1,5 +1,8 @@
 #include "Controler.h"
 
+Controler::Controler(): fpsArray(60, 0) {
+}
+
 void Controler::onCollision(const CollisionEvent&) {
 }
 
@@ -13,6 +16,14 @@ void Controler::start(Entity entity) {
 }
 
 void Controler::update(Entity entity) {
+	fps -= fpsArray[fpsIndex] / 60;
+	fpsArray[fpsIndex] = 1/getDeltaTime();
+	fps += fpsArray[fpsIndex] / 60;
+	fpsIndex = (fpsIndex + 1) % 60;
+	std::string fpsString = "FPS: ";
+	fpsString += char(fps / 10) % 100 + '0';
+	fpsString += char(fps) % 10 + '0';
+	drawText(fpsString, {10, 10, 1});
 	Transform& tf = getComponent<Transform>(entity.getId());
 	tf.velocity.x = (this->keyDown('d') - this->keyDown('a')) * 3;
 	tf.velocity.y = (this->keyDown('s') - this->keyDown('w')) * 3;
--- TestGame/Controler.h
+++ TestGame/Controler.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <System.h>
 class Controler :  public System {
+	DynamicArray<float> fpsArray;
+	float fps = 0;
+	int fpsIndex = 0;
 public:
+	Controler();	
 	void onCollision(const CollisionEvent&);
 	void start(Entity);
 	void update(Entity);
